705ae1ec2f7bac7c2f0cd570befd14706ae86a62
marcinn <01171345@pw.edu.pl>
2024-02-04 21:33:51 +0100

    quick fix

--- OfficelyMobileApp/API/OfficeStore.js
+++ OfficelyMobileApp/API/OfficeStore.js
@@ -14,7 +14,6 @@ const OfficeStore = create((set) => ({
           headers: {
             'Accept': '*/*',
             'Content-Type': 'application/json',  
-            'Authorization': `Bearer ${LoginStore.getState().jwttoken}`
       }}),
   fetchOffice:
       async (officeId) => fetch(`${url}/offices/${officeId}`, {
@@ -22,7 +21,6 @@ const OfficeStore = create((set) => ({
         headers: {
           'Accept': '*/*', 
           'Content-Type': 'application/json',  
-          'Authorization': `Bearer ${LoginStore.getState().jwttoken}`
       }}),
   saveOffice:
       async (officeId) => fetch(`${url}/offices/${officeId}/save`, {
